1a0b913ca6e85cc1075fbd4103d0fbba45b1ce55
Abramov Iurii <abtank@outlook.com>
2020-08-30 02:05:51 +0300

    product view list update delete create

---
+++ less2/src/main/java/ru/geek/controller/ProductController.java
@@ -0,0 +1,55 @@
+package ru.geek.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.geek.persistance.Product;
+import ru.geek.persistance.ProductRepository;
+import ru.geek.persistance.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public String allProducts(Model model) throws SQLException {
+        List<Product> allProducts = productRepository.getAllProducts();
+        model.addAttribute("products",allProducts);
+        return "products";
+    }
+
+    @GetMapping("/{id}")
+    public String editProduct(@PathVariable("id") Long id, Model model) throws SQLException {
+        Product product = productRepository.findById(id);
+        model.addAttribute("product", product);
+        return "product";
+    }
+
+    @PostMapping("/update")
+    public String updateProduct(Product product) throws SQLException {
+        productRepository.update(product);
+        return "redirect:/product";
+    }
+
+    @PostMapping("/create")
+    public String insertProduct(Product product) throws SQLException {
+        productRepository.insert(product);
+        return "redirect:/product";
+    }
+
+    @PostMapping("/delete")
+    public String deleteProduct(Product product) throws SQLException {
+        productRepository.delete(product);
+        return "redirect:/product";
+    }
+}
