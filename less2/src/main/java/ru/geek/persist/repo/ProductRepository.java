12b90f02cc66412f12a9dfdc5a1af9631265b613
Abramov Iurii <abtank@outlook.com>
2020-09-18 03:42:25 +0300

    little changes

--- less2/src/main/java/ru/geek/persist/repo/ProductRepository.java
+++ less2/src/main/java/ru/geek/persist/repo/ProductRepository.java
@@ -1,6 +1,8 @@
 package ru.geek.persist.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.geek.persist.entity.Product;
 
@@ -8,16 +10,25 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository <Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     List<Product> findByName(String name);
 
     List<Product> findByNameLike(String name);
+
     List<Product> findByPriceLike(BigDecimal price);
+
     List<Product> OrderByPriceDesc();
-    List<Product> findByPriceBetweenOrderByPriceDesc(BigDecimal minPrice,BigDecimal maxPrice);
+
+    List<Product> findByPriceBetweenOrderByPriceDesc(BigDecimal minPrice, BigDecimal maxPrice);
 
     List<Product> OrderByPrice();
 
     List<Product> findByNameLikeAndPriceLike(String NameLike, BigDecimal priceLike);
+
+    @Query("from User u" +
+            "where (u.name = :name or :name is null) and" +
+            "(u.price = price or :price is null)")
+    List<Product> quweryByNameLikeAndPriceLike(@Param("name") String NameLike, @Param("price") BigDecimal priceLike);
+
 }
--- less2/src/main/java/ru/geek/persist/repo/UserRepository.java
+++ less2/src/main/java/ru/geek/persist/repo/UserRepository.java
@@ -1,6 +1,8 @@
 package ru.geek.persist.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.geek.persist.entity.User;
 
@@ -12,4 +14,15 @@ public interface UserRepository extends JpaRepository <User,Integer> {
     List<User> findByLogin(String login);
 
     List<User> findByLoginLike(String loginPattern);
+
+    List<User> findByEmailLike(String email);
+
+    List<User> findByEmailLikeAndLoginLike(String login, String email);
+
+    @Query("FROM User u "+
+    "WHERE (u.email = :email or :email is null) and"+
+            "(u.login = :login or :login is null)")
+    List<User> queryByEmailLikeAndLoginLike(@Param("login") String login,@Param("email") String email);
+
+
 }
