70c0dbae87054db483a82037411c6f4bad49acd4
Abramov Iurii <abtank@outlook.com>
2020-09-23 02:38:36 +0300

    Conditional

--- less6/src/main/java/ru/geek/Less6Application.java
+++ less6/src/main/java/ru/geek/Less6Application.java
@@ -2,9 +2,17 @@ package ru.geek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Less6Application {
+
+	@Bean
+	@ConditionalOnProperty(name = "my.property", havingValue = "123")
+	public Object someBean(){
+		return new Object();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Less6Application.class, args);
